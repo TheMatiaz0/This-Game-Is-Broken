0eaaa90a2528ce234121095f59879974e782f951
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-01-31 19:26:04 +0100

    Simple Movement

---
+++ GameJamGame/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cyberevolver.Unity;
+
+public class PlayerMovement : MonoBehaviourPlus
+{
+    [field: SerializeField, Cyberevolver.Unity.MinMaxRange(0f, 0.8f)]
+    public float MovementSpeed { get; private set; } = 0.6f;
+
+    [Auto]
+    public Rigidbody2D Rb2D { get; set; }
+
+    private float move;
+
+    protected void Update()
+    {
+        move = Input.GetAxisRaw("Horizontal") * MovementSpeed;
+    }
+
+    protected void FixedUpdate()
+    {
+        // returns -1 to 1 multiplied by speed
+        Rb2D.MovePosition((Vector2)transform.position + move * Vector2.right);
+    }
+}
