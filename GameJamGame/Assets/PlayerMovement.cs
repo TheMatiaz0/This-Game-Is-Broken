1901cecb6940ed6587e625d73814d5be81565309
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-01-31 20:14:34 +0100

    Jumping - WiP

--- GameJamGame/Assets/PlayerMovement.cs
+++ GameJamGame/Assets/PlayerMovement.cs
@@ -6,7 +6,10 @@ using Cyberevolver.Unity;
 public class PlayerMovement : MonoBehaviourPlus
 {
     [field: SerializeField, Cyberevolver.Unity.MinMaxRange(0f, 0.8f)]
-    public float MovementSpeed { get; private set; } = 0.6f;
+    public float MovementSpeed { get; private set; } = 0.11f;
+
+    [field: SerializeField, Cyberevolver.Unity.MinMaxRange(0f, 400f)]
+    public float JumpMultiple { get; private set; }
 
     [Auto]
     public Rigidbody2D Rb2D { get; set; }
@@ -16,6 +19,19 @@ public class PlayerMovement : MonoBehaviourPlus
     protected void Update()
     {
         move = Input.GetAxisRaw("Horizontal") * MovementSpeed;
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump ()
+    {
+        Rb2D.velocity = Vector3.up * JumpMultiple;
+
+        // Rb2D.AddForce(Vector3.up * JumpMultiple);
     }
 
     protected void FixedUpdate()
