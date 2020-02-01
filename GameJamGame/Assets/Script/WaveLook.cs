835372c13c4fd0e97a1aac24315f22e7a73f9329
Biegus <48364457+Biegus@users.noreply.github.com>
2020-02-01 17:03:40 +0100

    Update WaveLook.cs

--- GameJamGame/Assets/Script/WaveLook.cs
+++ GameJamGame/Assets/Script/WaveLook.cs
@@ -12,4 +12,37 @@ using Cyberevolver.Unity;
 public class WaveLook : MonoBehaviourPlus
 {
 
+    [SerializeField]
+    [AssetOnly]
+    private SpriteRenderer prefab;
+    
+
+    [SerializeField]
+    private Sprite[] sprites;
+    [SerializeField]
+    private Vector2 elementSize;
+    [SerializeField]
+    private Vector2Int elementsQuanity;
+
+
+
+    private SpriteRenderer[] elements;
+
+
+    private void Generate()
+    {
+        for(int x=0;x<elements.x;x++)
+            for(int y=0;y<elements.y;y++)
+            {
+                var obj = Instantiate(prefab, (Vector2)this.transform.position + new Vector2(x, y), Quaternion.identity);
+
+            }
+    }
+    protected override void OnGUI()
+    {
+        base.OnGUI();
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(this.transform.position, elements * elementSize);
+
+    }
 }
