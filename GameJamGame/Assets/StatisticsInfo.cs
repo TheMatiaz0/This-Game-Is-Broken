763ce2710d2c975b44abed3e173f52539bbb4173
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-02-01 20:04:00 +0100

    Fix Statistics

--- GameJamGame/Assets/StatisticsInfo.cs
+++ GameJamGame/Assets/StatisticsInfo.cs
@@ -5,21 +5,26 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StatisticsInfo : MonoBehaviour
+public class StatisticsInfo : MonoBehaviourPlus
 {
 	[Auto]
 	public Text statisticsText { get; private set; }
 
 	private Stopwatch timer;
 
-	protected void Awake()
+	protected new void Awake()
 	{
+		base.Awake();
 		timer = new Stopwatch();
 		timer.Start();
 	}
 
 	protected void Update ()
 	{
-		statisticsText.text = $"<color=red>{timer.Elapsed.TotalHours}h, {timer.Elapsed.TotalMinutes}m, {timer.Elapsed.TotalSeconds}s</color> | <color=yellow>900 meters</color>";
+		if (timer != null && timer.IsRunning)
+		{
+			statisticsText.text = $"<color=red>{timer.Elapsed.Hours}h, {timer.Elapsed.Minutes}m, {timer.Elapsed.Seconds}s</color> | <color=yellow>900 meters</color>";
+		}
+		
 	}
 }
