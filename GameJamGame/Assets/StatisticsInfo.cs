79f64f285f47d7e4d165fe66cf35fb39ff07b8f4
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-02-01 19:54:51 +0100

    Statistics++

---
+++ GameJamGame/Assets/StatisticsInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatisticsInfo : MonoBehaviour
+{
+	[SerializeField]
+	private Text statisticsText = null;
+
+	private Stopwatch timer;
+
+	protected void Awake()
+	{
+		timer = new Stopwatch();
+		timer.Start();
+	}
+
+	protected void Update ()
+	{
+		statisticsText.text = $"<color=red>{timer.Elapsed.TotalHours}h, {timer.Elapsed.TotalMinutes}m, {timer.Elapsed.TotalSeconds}s</color> | <color=yellow>900 meters</color>";
+	}
+}
