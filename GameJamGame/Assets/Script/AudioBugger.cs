6cc40f60dbd115facc05757a70878e8386e4dab3
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-02-02 12:56:37 +0100

    Cleaning up

---
+++ GameJamGame/Assets/Script/AudioBugger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBugger : GlitchEffect
+{
+	
+	public override string Description => "AUDIO_DRIVER_CRASH";
+
+	protected override void OnCancel()
+	{
+	
+	}
+
+	public override void WhenCollect()
+	{
+		
+	}
+}
