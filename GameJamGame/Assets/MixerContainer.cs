ba3078932bdf4f1ca073e7bc0f2823943d74d255
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-02-02 13:08:44 +0100

    Fix and not fix

--- GameJamGame/Assets/MixerContainer.cs
+++ GameJamGame/Assets/MixerContainer.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MixerContainer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public AudioMixer Mixer { get; private set; }
 }
