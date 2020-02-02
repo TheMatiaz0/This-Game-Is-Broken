ad439e90002cb51f023d0297674e4f5190085bbb
Biegus <48364457+Biegus@users.noreply.github.com>
2020-02-02 01:25:23 +0100

    Clean after reversing

--- GameJamGame/Assets/Script/ExplosionElement.cs
+++ GameJamGame/Assets/Script/ExplosionElement.cs
@@ -26,7 +26,8 @@ public class ExplosionElement : MonoBehaviourPlus
                 + Vector2.up * 150);
             LeanTween.alpha(bug.gameObject, 0, 2).setOnComplete(() => Destroy(bug.gameObject));
             bug.OnExplode();
-            
+            bug.SpawnDeathParticles();
+
         }
     }
     private void Update()
