3a44f84c3ffe3406a3f24dd0e0724da4aaf647be
Biegus <48364457+Biegus@users.noreply.github.com>
2020-02-02 01:11:04 +0100

    Revert "Merge branch 'master' of https://github.com/TheMatiaz0/Unnamed-Global-Game-Jam-Project"
    This reverts commit 31a8f54ddb51952df62f0ec0b42215b7c0352260, reversing
    changes made to 933fa31fda32cfb9d1f74f99f20d024f4d72ac54.

--- GameJamGame/Assets/Script/Wave.cs
+++ GameJamGame/Assets/Script/Wave.cs
@@ -54,13 +54,6 @@ public class Wave : ActiveElement
         
         player.Kill();
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            Destroy(gameObject);
-        }
-    }
    
 
 
