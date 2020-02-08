d268d0dab27e1c645d808e95c8fa0e0d0aec62d7
Biegus <michalbiegaj0@wp.pl>
2020-02-09 00:06:56 +0100

    Spike fix

--- GameJamGame/Assets/Spike.cs
+++ GameJamGame/Assets/Spike.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spike : Collectable
+public class Spike : ActiveElement
 {
     public override bool IsBad =>true;
-    protected override void OnCollect()
+    protected override void OnColidWithPlayer(PlayerController player)
     {
         PlayerController.Instance.Kill();
+        
     }
 
 }
