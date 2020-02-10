4bc31f234a70e5ccdf0d711f1c974843878a0f41
kurroneko <kurroneko01@gmail.com>
2020-02-10 12:44:55 +0100

    added tutorial

---
+++ GameJamGame/Assets/Script/TutorialScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//watch this first: https://www.youtube.com/watch?v=0VGosgaoTsw
+
+public class TutorialScript : MonoBehaviour
+{
+    public float tutorialSpeed;
+
+    public GameObject myObject;
+
+       void Start()
+    {
+        //tutorialSpeed = Time.timeScale;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = tutorialSpeed;
+        
+
+        if (Input.GetKeyDown("return"))
+        {
+            tutorialSpeed = 1f;
+            myObject.SetActive(false);
+        }
+        else
+        {
+            myObject.SetActive(true);
+            tutorialSpeed = 0.1f;
+        }
+    }
+}
