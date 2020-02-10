356b14d62c91fe022da9fa7af12c279bc65d68ae
Biegus <michalbiegaj0@wp.pl>
2020-02-10 20:16:54 +0100

    changes

--- GameJamGame/Assets/CyberExt2/Editor/EditorP/CyberInspector.cs
+++ GameJamGame/Assets/CyberExt2/Editor/EditorP/CyberInspector.cs
@@ -10,6 +10,7 @@ using System.Collections;
 using UnityEditor;
 namespace  Cyberevolver.EditorUnity
 {
+    [CanEditMultipleObjects]
     [CustomEditor(typeof(UnityEngine.Object),true)]
     public class CyberInspector : Editor
     {
